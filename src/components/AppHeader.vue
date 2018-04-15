8c59ae36834fb09d41efcc54bfe79c742d09e424
mercedesb <mercedesbernard@live.com>
2018-04-15 08:50:11 -0500

    fixed spacing and exploring font faces

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class='MainNav'>
-    <router-link to="/"><h1>Mercedes Bernard</h1></router-link>
+    <router-link to="/"><img class='MainNav-logo' src='../assets/initials.svg' /></router-link>
     <!-- <navigation-links></navigation-links> -->
   </header>
 </template>
