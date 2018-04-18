7d5cc21cb100fd31ffb073e0de8036f2f1e125ce
mercedesb <mercedesbernard@live.com>
2018-04-18 09:55:31 -0500

    wip: blog post

--- src/components/pages/BlogPostPage.vue
+++ src/components/pages/BlogPostPage.vue
@@ -1,11 +1,65 @@
 <template>
-  <div>
-  Woo! blog post page!
-</div>
+   <div>
+    <PageHeader
+      :color="page.color || 'brown'"
+      :title="page.title"
+      :preamble="page.preamble"
+      :media="iconUrl"></PageHeader>
+    <div v-if="page.mainContent" class='FlexContainer FlexContainer--justifyCenter'>
+      <p class="PageContent">
+        <parse-markdown :source="page.mainContent" />
+      </p>
+    </div>
+    <div v-if="ctaLinks.length" class='FlexContainer FlexContainer--justifyCenter'>
+      <div class='PageContent PageContent--wide FlexContainer FlexContainer--wrap'>
+      <CTALink
+        v-for="ctaLink in ctaLinks"
+        :key="ctaLink.id"
+        v-bind="ctaLink"
+      />
+    </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import PageHeader from '../PageHeader.vue'
+import CTALink from '../CTALink.vue'
+
 export default {
+  components: {
+    PageHeader, CTALink
+  },
+  props: {
+    urlSegment: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    page() {
+      return this.$store.state.landingPage
+    },
+    iconUrl() {
+      return this.getImageUrl(this.page.icon)
+    },
+    ctaLinks() {
+      if (!this.page.ctaLinks) return []
+      return this.page.ctaLinks.map((cta) => {
+        return {
+          id: cta.sys.id,
+          color: this.page.color,
+          title: cta.fields.title,
+          icon: this.getImageUrl(cta.fields.icon),
+          url: cta.fields.externalLink || cta.fields.internalLink.fields.urlSegment,
+          external: !!cta.fields.externalLink
+        }
+      })
+    }
+  },
+  created () {
+    this.$store.dispatch('getBlogPost', this.urlSegment)
+  }
 }
 </script>
 
