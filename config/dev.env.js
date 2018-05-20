4db69ecf6f7b6aa6e34c2d6eee61a7d60dbadd6c
mercedesb <mercedesbernard@live.com>
2018-05-20 15:47:47 -0500

    configure server for heroku deployments

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,11 +1,7 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
-require('dotenv').config()
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  SPACE_ID: JSON.stringify(process.env.SPACE_ID),
-  CDA_TOKEN: JSON.stringify(process.env.CDA_TOKEN),
-  MAILER_API: JSON.stringify(process.env.MAILER_API)
+  NODE_ENV: '"development"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,9 @@
 'use strict'
+require('dotenv').config()
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  SPACE_ID: JSON.stringify(process.env.SPACE_ID),
+  CDA_TOKEN: JSON.stringify(process.env.CDA_TOKEN),
+  MAILER_API: JSON.stringify(process.env.MAILER_API)
 }
