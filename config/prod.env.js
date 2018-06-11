f62a787f86670416ac02ab1cb23a4908ef0f9b24
mercedesb <mercedesbernard@live.com>
2018-06-10 19:51:42 -0500

    updated mailer api path for prod

--- config/prod.env.js
+++ config/prod.env.js
@@ -5,5 +5,5 @@ module.exports = {
   NODE_ENV: '"production"',
   SPACE_ID: JSON.stringify(process.env.SPACE_ID),
   CDA_TOKEN: JSON.stringify(process.env.CDA_TOKEN),
-  MAILER_API: JSON.stringify(process.env.MAILER_API)
+  MAILER_API: '"https://mercedes-utils.herokuapp.com/utilities/send"'
 }
