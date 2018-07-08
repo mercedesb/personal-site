e83b4281f5ff5bb93fe82a774b387a8d140e0ed7
mercedesb <mercedesbernard@live.com>
2018-07-08 13:29:09 -0500

    added logging statements

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,17 +3,22 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  echo "git configured"
 }
 
 commit_website_files() {
   git checkout -b production
   git add .
+  echo "added"
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  echo "committed"
 }
 
 upload_files() {
   git remote add origin https://${GH_TOKEN}@github.com/mercedesb/personal-site.git > /dev/null 2>&1
+  echo "remote added"
   git push --quiet --set-upstream origin production
+  echo "pushed"
 }
 
 setup_git
