12c84d7eddc7d81a46187dcf64144f36fa63981a
mercedesb <mercedesbernard@live.com>
2018-07-18 11:24:04 -0500

    update env for dev

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  MAILER_API: '"http://localhost:8091/utilities/send"'
 })
