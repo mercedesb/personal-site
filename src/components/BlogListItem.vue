a5b31b4c11de0fb267d41f29e242d1f7eff62340
Mercedes Bernard <mercedesrbernard@gmail.com>
2019-04-07 19:32:25 -0500

    small accessibility update

--- src/components/BlogListItem.vue
+++ src/components/BlogListItem.vue
@@ -7,7 +7,7 @@
         <span>{{ publishYear }}</span>
       </div>
       <div class='BlogItem-text'>
-      <h5 class='BlogItem-title'>{{ title }}</h5>
+      <h3 class='BlogItem-title h5'>{{ title }}</h3>
       <p class='BlogItem-preamble'>{{ preamble }}</p>
       <p class='BlogItem-viewMore'>
         View more |
