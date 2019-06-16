2d74d06f3deb4625d8f5a8de7ae74aca7ce95d64
Mercedes Bernard <mercedesrbernard@gmail.com>
2019-06-15 19:19:59 -0500

    configure prerendering
    Make a local plugin to write your own wrapper around build and do the async
    fetching of data before actually running the prerender and build
    (see this issue: https://github.com/vuejs/vue-cli/issues/3328)

---
+++ build.prerender.js
@@ -0,0 +1,63 @@
+const path = require("path");
+
+function getClient() {
+  return require("contentful").createClient({
+    space: process.env.VUE_APP_SPACE_ID,
+    accessToken: process.env.VUE_APP_CDA_TOKEN
+  });
+}
+
+async function getBlogRoutes() {
+  const client = getClient();
+
+  return client
+    .getEntries({
+      content_type: "blogPost"
+    })
+    .then(response =>
+      response.items.map(item => `/blog/${item.fields.urlSegment}`)
+    );
+}
+
+async function getSpeakingRoutes() {
+  const client = getClient();
+
+  return client
+    .getEntries({ content_type: "talkPage" })
+    .then(response =>
+      response.items.map(item => `/speaking/${item.fields.urlSegment}`)
+    );
+}
+
+module.exports = (api, _options) => {
+  api.registerCommand("build:prerender", async args => {
+    const PrerenderSPAPlugin = require("prerender-spa-plugin");
+    const PuppeteerRenderer = PrerenderSPAPlugin.PuppeteerRenderer;
+
+    const defaultRoutes = ["/", "/about", "/speaking", "/blog", "/resume"];
+    const blogRoutes = await getBlogRoutes();
+    const speakingRoutes = await getSpeakingRoutes();
+    const allRoutes = defaultRoutes.concat(blogRoutes).concat(speakingRoutes);
+
+    api.chainWebpack(config => {
+      config.plugin("prerender").use(PrerenderSPAPlugin, [
+        {
+          // Required - The path to the webpack-outputted app to prerender.
+          staticDir: path.join(__dirname, "dist"),
+          // Required - Routes to render.
+          routes: allRoutes,
+          renderer: new PuppeteerRenderer({
+            // renderAfterDocumentEvent: 'custom-render-trigger',
+            renderAfterTime: 5000
+          })
+        }
+      ]);
+    });
+
+    await api.service.run("build", args);
+  });
+};
+
+module.exports.defaultModes = {
+  "build:prerender": "production"
+};
