b5cf0f329eb5c80dae216b576ccbec106e061c61
Mercedes Bernard <mercedesrbernard@gmail.com>
2019-06-16 09:30:35 -0500

    Fix travis lint command and fix style
    `npm run lint` instead of `npm lint`
    
    Make the header a little taller on blog pages to account for nav hover
    on desktop.

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -42,7 +42,7 @@ export default {
   @import '../assets/styles/variables.scss';
 
   $header-height: 450px;
-  $header-height-short: 275px;
+  $header-height-short: 325px;
 
   $media-width: 800px;
   $text-width: 500px;
