e7a51c6279b645fbc67284def440cd5a7a8a736e
Mercedes Bernard <mercedesrbernard@gmail.com>
2019-09-23 19:57:21 -0500

    Refactor blog post page into wrapper and presentational components

--- src/components/pages/BlogPostPage.vue
+++ src/components/pages/BlogPostPage.vue
@@ -33,71 +33,19 @@
 import PageHeader from '../PageHeader.vue'
 import ParseMarkdown from '../ParseMarkdown.vue'
 import ReadingTime from '../ReadingTime.vue'
-import objects from '../../mixins/objects'
-import images from '../../mixins/images'
-
-const moment = require('moment')
 
 export default {
   components: {
     PageHeader, ParseMarkdown, ReadingTime
   },
-  mixins: [
-    objects, images
-  ],
-  metaInfo () {
-    return {
-      title: this.page.title,
-      titleTemplate: '%s | Mercedes Bernard',
-      meta: [
-        { name: 'description', vmid: 'description', content: this.page.preamble },
-        { name: 'og:title', vmid: 'og:title', content: this.page.title },
-        // Twitter Card data
-        { name: 'twitter:card', vmid: 'twitter:card', content: 'summary' },
-        { name: 'twitter:site', vmid: 'twitter:site', content: '@mercedescodes' },
-        { name: 'twitter:title', vmid: 'twitter:title', content: this.page.title },
-        { name: 'twitter:description', vmid: 'twitter:description', content: this.page.preamble },
-        { name: 'twitter:creator', vmid: 'twitter:creator', content: '@mercedescodes' },
-        { name: 'twitter:image', content: this.socialImage ? `${window.location.protocol}${this.socialImage}?w=800&fit=fill` : '' },
-        // Open Graph data
-        { property: 'og:title', vmid: 'og:title', content: this.page.title },
-        { property: 'og:type', vmid: 'og:type', content: 'article' },
-        { property: 'og:url', vmid: 'og:url', content: `http://www.mercedesbernard.com${this.$route.fullPath}/` },
-        { property: 'og:image', content: this.socialImage ? `${window.location.protocol}${this.socialImage}?w=800&fit=fill` : '' },
-        { property: 'og:description', vmid: 'og:description', content: this.page.preamble },
-        { property: 'og:site_name', vmid: 'og:site_name', content: 'Mercedes Bernard' }
-      ]
-    }
-  },
   props: {
+    page: Object,
+    formattedPublishDate: String,
+    socialImage: String,
     color: {
       type: String,
       default: 'purple'
     }
-  },
-  computed: {
-    page () {
-      return this.$store.state.blogPost
-    },
-    formattedPublishDate () {
-      return moment(this.page.publishDate).format('MMM DD YYYY')
-    },
-    socialImage () {
-      return this.getImageUrl(this.page.socialImage)
-    }
-  },
-  created () {
-    this.$store.dispatch('getBlogPost', this.$route.params.urlSegment)
-      .then((page) => {
-        if (this.isEmpty(page)) {
-          this.$router.push({name: 'pageNotFound'})
-        }
-
-        this.$store.dispatch('getNavLinks')
-          .then(() => {
-            document.dispatchEvent(new Event('custom-render-trigger'))
-          })
-      })
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,39 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-import HomePage from '../components/pages/HomePage.vue'
-import BlogPostPage from '../components/pages/BlogPostPage.vue'
-import TalkPage from '../components/pages/TalkPage.vue'
-import LandingPage from '../components/pages/LandingPage.vue'
-import PageNotFound from '../components/pages/PageNotFound.vue'
+import HomePage from "../components/pages/HomePage.vue";
+import BlogPostPageContainer from "../components/pages/BlogPostPageContainer.vue";
+import TalkPage from "../components/pages/TalkPage.vue";
+import LandingPage from "../components/pages/LandingPage.vue";
+import PageNotFound from "../components/pages/PageNotFound.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   scrollBehavior: () => ({ y: 0 }),
   routes: [
-    { path: '/', component: HomePage },
-    { path: '/blog/:urlSegment', name: 'blogPost', component: BlogPostPage, props: true },
-    { path: '/speaking/:urlSegment', name: 'talkPage', component: TalkPage, props: true },
+    { path: "/", component: HomePage },
     {
-      path: '/:urlSegment',
+      path: "/blog/:urlSegment",
+      name: "blogPost",
+      component: BlogPostPageContainer,
+      props: true
+    },
+    {
+      path: "/speaking/:urlSegment",
+      name: "talkPage",
+      component: TalkPage,
+      props: true
+    },
+    {
+      path: "/:urlSegment",
       component: LandingPage,
-      props: (route) => ({
+      props: route => ({
         ...route.params,
         ...route.query
       })
     },
-    { path: '/404', alias: '*', name: 'pageNotFound', component: PageNotFound }
+    { path: "/404", alias: "*", name: "pageNotFound", component: PageNotFound }
   ]
-})
+});
